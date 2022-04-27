b09a4914b468a60a63ff7e951d5b3c84d7da32f0
Atanas Pavlov <nasko.js@gmail.com>
2022-04-27 16:51:28 +0300

    Initial commit

---
+++ scala/distributed_systems/ds_lib/src/main/scala/ds/MapReduceSerial.scala
@@ -0,0 +1,16 @@
+package ds
+
+import ds.mr.{IMapReduce, KeyValue}
+import ds.mr.apps.WordCount
+
+object MapReduceSerial {
+  def map_reduce[A](app: IMapReduce[A], inputs: Seq[(String, String)]): Seq[(String, A)] = {
+    type VType = A
+    type KVType = KeyValue[A]
+    val intermediate: Seq[KVType] = inputs.flatMap(input => app.map(input._1, input._2))
+    val sorted: Seq[(String, Seq[VType])] = intermediate.groupMap(x => x.key)(kv => kv.value).toSeq
+    val result = sorted.map(item => (item._1, app.reduce(item._1, item._2)))
+    result
+  }
+
+}
